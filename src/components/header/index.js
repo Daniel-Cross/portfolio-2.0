8b89bbd8faf9a86b5ec84f79929d089abdc44974
Daniel Cross <crossy1686@gmail.com>
2019-03-22 20:45:43 +0100

    initial commit

---
+++ src/components/header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../styles/header/index.css';
+
+const Header = () => {
+	return (
+		<div className="Header">
+			<h1>DANIEL CROSS</h1>
+			<hr />
+			<h3>SOFTWARE ENGINEER</h3>
+		</div>
+	);
+};
+
+export default Header;
